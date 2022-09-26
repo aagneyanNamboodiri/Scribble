f3e912ff777248562173e2d913e686ff1d21b122
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-09-26 10:48:10 +0530

    Added slug to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,27 @@ class Article < ApplicationRecord
   MAX_BODY_LENGTH = 5000
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :title, presence: true, length: { maximum: MAX_BODY_LENGTH }
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      itr = 1
+      loop do
+        title_slug = title.parameterize
+        slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
+        break self.slug = slug_candidate unless Article.exists?(slug: slug_candidate)
+
+        itr += 1
+      end
+    end
+
+    def slug_not_changed
+      if slug_changed? && self.persisted?
+        errors.add(:slug, "is immutable!")
+      end
+    end
 end
