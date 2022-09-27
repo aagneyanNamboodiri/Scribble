e63a443d64ca0ea338198c4e8a0462b3beebd065
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-09-27 16:37:03 +0530

    Added controller for categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :articles, only: %i[index create show], param: :slug, defaults: { format: "json" }
+  resources :categories, only: %i[index create], defaults: { format: "json" }
 
   root "home#index"
   get "*path", to: "home#index", via: :all
