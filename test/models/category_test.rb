e9fbfab6a05eb97672073e855c6a12f3445ed5da
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-09-29 12:44:49 +0530

    Setup FactoryBot and Faker and added tests to Category model

---
+++ test/models/category_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class CategoryTest < ActiveSupport::TestCase
+  def setup
+    @category = build(:category)
+  end
+
+  def test_category_should_not_be_valid_and_saved_without_category_name
+    @category.name = ""
+    assert_not @category.valid?
+    assert_includes @category.errors.full_messages, "Name can't be blank"
+  end
+
+  def test_category_can_have_zero_articles_count
+    @category.articles_count = 0
+    assert @category.valid?
+  end
+end
