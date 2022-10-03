b4555a84a6e7a3d6532a62c92beee663886fbaf2
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-03 15:26:04 +0530

    Added validation schema to form

--- app/javascript/src/components/Dashboard/ArticlesForm/Form.jsx
+++ app/javascript/src/components/Dashboard/ArticlesForm/Form.jsx
@@ -7,6 +7,11 @@ import { useHistory } from "react-router-dom";
 
 import articlesApi from "apis/articles";
 
+import {
+  buildArticlesFormValidationSchema,
+  INITIAL_ARTICLES_FORM_VALUES,
+} from "./constants";
+
 const ArticleForm = ({ categories }) => {
   const history = useHistory();
   const { Menu, MenuItem } = ActionDropdown;
@@ -21,8 +26,7 @@ const ArticleForm = ({ categories }) => {
       await articlesApi.create(modifiedValues);
       history.push("/dashboard");
     } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log(err);
+      logger.log(err);
     }
   };
   const categoryList = categories.map(category => ({
@@ -32,19 +36,14 @@ const ArticleForm = ({ categories }) => {
 
   return (
     <Formik
-      initialValues={{
-        title: "",
-        body: "",
-        category: "",
-        status: "draft",
-      }}
+      initialValues={INITIAL_ARTICLES_FORM_VALUES}
+      validationSchema={buildArticlesFormValidationSchema(categoryList)}
       onSubmit={handleSubmit}
     >
       {({ isSubmitting, handleSubmit }) => (
-        <Form className="mt-2 w-full space-y-4">
-          <div className="flex w-full space-x-2">
+        <Form className="mx-24 space-y-4 p-20 px-56">
+          <div className="flex space-x-2">
             <Input
-              className="w-2/3 flex-grow-0"
               label="Article Title"
               name="title"
               placeholder="Title of your article"
@@ -53,7 +52,6 @@ const ArticleForm = ({ categories }) => {
             <Select
               isClearable
               isSearchable
-              className="w-1/3 flex-grow-0"
               label="Category"
               name="category"
               options={categoryList}
@@ -67,41 +65,36 @@ const ArticleForm = ({ categories }) => {
             rows="15"
             // onChange={e => setBody(e.target.value)}
           />
-          <div className="flex items-start">
-            <div className="mr-4">
-              <ActionDropdown
-                disabled={isSubmitting}
-                label={status === "draft" ? "Save Draft" : "Publish"}
-                loading={isSubmitting}
-                type="submit"
-                onClick={handleSubmit}
-              >
-                <Menu>
-                  {statusList.map((item, idx) => (
-                    <MenuItem.Button
-                      key={idx}
-                      onClick={() => {
-                        item === "Save Draft"
-                          ? setStatus("draft")
-                          : setStatus("published");
-                      }}
-                    >
-                      {item}
-                    </MenuItem.Button>
-                  ))}
-                </Menu>
-              </ActionDropdown>
-            </div>
-            <div>
-              <Button
-                className="grow-0"
-                label="Cancel"
-                size="large"
-                style="text"
-                type="reset"
-                onClick={() => history.push("/dashboard")}
-              />
-            </div>
+          <div className="mr-4 space-x-2">
+            <ActionDropdown
+              disabled={isSubmitting}
+              label={status === "draft" ? "Save Draft" : "Publish"}
+              loading={isSubmitting}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              <Menu>
+                {statusList.map((item, idx) => (
+                  <MenuItem.Button
+                    key={idx}
+                    onClick={() => {
+                      item === "Save Draft"
+                        ? setStatus("draft")
+                        : setStatus("published");
+                    }}
+                  >
+                    {item}
+                  </MenuItem.Button>
+                ))}
+              </Menu>
+            </ActionDropdown>
+            <Button
+              label="Cancel"
+              size="large"
+              style="text"
+              type="reset"
+              onClick={() => history.push("/dashboard")}
+            />
           </div>
         </Form>
       )}
