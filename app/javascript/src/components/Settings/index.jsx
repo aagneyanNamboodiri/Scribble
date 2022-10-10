6a2fc1c7cc36e22fbd0f1e1da7a347434fe0af27
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-10 15:15:34 +0530

    Added a route and component for redirections

--- app/javascript/src/components/Settings/index.jsx
+++ app/javascript/src/components/Settings/index.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
+// eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
 import { Redirect, Route } from "react-router";
 
 import Categories from "./Categories";
 import General from "./General";
+import Redirections from "./Redirections";
 import Sidebar from "./Sidebar";
 
 const Settings = () => (
   <div className="flex">
     <Sidebar />
-    <Route exact component={General} path="/settings/general" />
-    <Route exact component={Categories} path="/settings/categories" />
-    <Redirect from="/settings" to="/settings/general" />
+    <Route component={General} path="/settings/general" />
+    <Route component={Categories} path="/settings/categories" />
+    <Route component={Redirections} path="/settings/redirections" />
+    {/* <Redirect from="/settings" to="/settings/general" /> */}
   </div>
 );
 
