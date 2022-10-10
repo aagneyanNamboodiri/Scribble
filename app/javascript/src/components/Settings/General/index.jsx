bd4ae3374cf2094f4d5fd045bcdf5f0c804b7eb5
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-10 12:52:07 +0530

    Displaying data fetched from backend

--- app/javascript/src/components/Settings/General/index.jsx
+++ app/javascript/src/components/Settings/General/index.jsx
@@ -1,13 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { Typography, Input, Checkbox, Button } from "neetoui";
+import { Typography, Input, Checkbox, Button, PageLoader } from "neetoui";
+
+import preferencesApi from "apis/preferences";
 
 import ValidationMessages from "./ValidationMessages";
 
 const General = () => {
   const [checked, setChecked] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [password, setPassword] = useState("");
   const [isPasswordValid, setIsPasswordValid] = useState([1, 1]);
+  const [preferences, setPreferences] = useState({});
+  const [siteName, setSiteName] = useState("");
+
+  const fetchPreferences = async () => {
+    try {
+      setLoading(true);
+      const { data } = await preferencesApi.list();
+      setPreferences(data);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const setSettingsValues = () => {
+    if (preferences.password_digest) setPassword(preferences.password_digest);
+    setChecked(preferences.is_password);
+    setSiteName(preferences.site_name);
+  };
+
+  useEffect(() => {
+    fetchPreferences();
+    setSettingsValues();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-full">
+        <PageLoader />
+      </div>
+    );
+  }
 
   //TODO : Clicking cancel button should revert the state back to what was fetched from DB
   //TODO : If password was set to blank and saved, password field checkbox should be unchecked
@@ -27,7 +63,8 @@ const General = () => {
           <Input
             helpText="Customize the sitename which is used to show the site name"
             label="Site Name"
-            value="Spinkart"
+            value={siteName}
+            onChange={e => setSiteName(e.target.value)}
           />
         </div>
         <hr />
