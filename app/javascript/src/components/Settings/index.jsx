bdb2ad2c54377613abc4fb5c216c66a6d579d519
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-10 09:37:31 +0530

    Added redirection to general

--- app/javascript/src/components/Settings/index.jsx
+++ app/javascript/src/components/Settings/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Route } from "react-router";
+import { Redirect, Route } from "react-router";
 
 import Categories from "./Categories";
 import General from "./General";
@@ -11,6 +11,7 @@ const Settings = () => (
     <Sidebar />
     <Route exact component={General} path="/settings/general" />
     <Route exact component={Categories} path="/settings/categories" />
+    <Redirect from="/settings" to="/settings/general" />
   </div>
 );
 
