742fa0b5c18d65a61fc0bb1b11fb6f800f9d2ad7
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-11 16:11:47 +0530

    Added preview button in dashboard Navbar

--- app/javascript/src/components/Navbar/index.jsx
+++ app/javascript/src/components/Navbar/index.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 
-import { Typography } from "neetoui";
+import { ExternalLink } from "neetoicons";
+import { Typography, Button } from "neetoui";
 
 import NavItem from "./NavItem";
 
 const Navbar = () => (
-  <div className="border-b flex p-5">
-    <Typography className="font-semibold" style="h3">
-      Scribble
-    </Typography>
-    <div className="pl-4">
-      <NavItem iconClass="ri-add-fill " name="Articles" path="/articles" />
-      <NavItem
-        iconClass="ri-file-download-fill"
-        name="Settings"
-        path="/settings"
-      />
+  <div className="border-b flex justify-between p-5">
+    <div className="flex">
+      <Typography className="font-semibold" style="h3">
+        Scribble
+      </Typography>
+      <div className="pl-4">
+        <NavItem iconClass="ri-add-fill " name="Articles" path="/articles" />
+        <NavItem
+          iconClass="ri-file-download-fill"
+          name="Settings"
+          path="/settings"
+        />
+      </div>
     </div>
+    <Button
+      icon={() => <ExternalLink size={17} />}
+      label="Preview"
+      style="secondary"
+      to="/public"
+    />
   </div>
 );
 
