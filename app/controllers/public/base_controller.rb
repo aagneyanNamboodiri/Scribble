4becf58029f5511c07ab3f01d2acc4a2b2d4396f
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-15 12:29:59 +0530

    Minor changes to code before merging PR

--- app/controllers/public/base_controller.rb
+++ app/controllers/public/base_controller.rb
@@ -3,8 +3,8 @@
 class Public::BaseController < ApplicationController
   def check_if_user_is_authorized
     preference = Preference.first
-    a = cookies[:auth] == preference.authentication_token
-    if a == false && preference.is_password
+    is_auth_token_set = cookies[:auth] == preference.authentication_token
+    if !is_auth_token_set && preference.is_password
       respond_with_error("Not authorized", :unauthorized)
     end
   end
