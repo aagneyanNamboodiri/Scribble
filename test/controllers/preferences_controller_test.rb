d1f2daad4dcc6f33301ed44702a3414dbe67d48b
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-17 16:07:03 +0530

    Added validations to preference model

--- test/controllers/preferences_controller_test.rb
+++ test/controllers/preferences_controller_test.rb
@@ -4,5 +4,13 @@ require "test_helper"
 
 class PreferencesControllerTest < ActionDispatch::IntegrationTest
   def setup
+    @preference = create(:preference)
+  end
+
+  def test_site_name_shouldnt_be_empty
+    @preference.site_name = ""
+    assert_raises ActiveRecord::RecordInvalid do
+      @preference.save!
+    end
   end
 end
