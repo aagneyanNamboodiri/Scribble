048e5b911daabb05489a34f19d2390f019a794dd
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-17 17:45:00 +0530

    Added article tests

--- test/controllers/public/articles_controller_test.rb
+++ test/controllers/public/articles_controller_test.rb
@@ -3,7 +3,32 @@
 require "test_helper"
 
 class Public::ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = build(:user)
+    @article = create(:article)
+    @preference = create(:preference, is_password: false)
+    @headers = headers()
+  end
+
+  def test_list_all_articles
+    get public_articles_path, headers: headers
+    assert_response :success
+
+    response_json = response.parsed_body
+    all_articles = response_json["articles"]
+    published_articles_count = Article.where(status: "published").count
+
+    assert_equal published_articles_count, Article.where(status: :published).count
+  end
+
+  def test_lists_correct_article
+    @article.save!
+    slug_to_get = @article.slug
+    get public_article_path(slug_to_get), headers: headers
+    assert_response :success
+
+    response_json = response.parsed_body
+    article_title = response_json["title"]
+    assert_equal @article.title, article_title
+  end
 end
