5856ae3235a62fada8ed61ed22e098a72f5cd4f1
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-17 17:54:42 +0530

    Added preferences controller tests

--- test/controllers/public/preferences_controller_test.rb
+++ test/controllers/public/preferences_controller_test.rb
@@ -3,7 +3,21 @@
 require "test_helper"
 
 class Public::PreferencesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @preference = create(:preference, is_password: false)
+    @headers = headers()
+  end
+
+  def test_get_correct_preference
+    get public_preferences_path, headers: headers
+    assert_response :success
+
+    response_json = response.parsed_body
+    site_name = response_json["site_name"]
+    password_digest = response_json["password_digest"]
+    current_preference = Preference.first
+    byebug
+    assert_equal site_name, current_preference.site_name
+    assert_equal password_digest, current_preference.password_digest
+  end
 end
