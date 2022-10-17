20c6a7be6d6ac9920f5d934c4d12ffe8cd628544
Aagneyan Namboodiri <110370191+aagneyanNamboodiri@users.noreply.github.com>
2022-10-17 17:55:38 +0530

    Public namespace controller tests (#83)
    * Added article tests
    
    * Added categories controller tests
    
    * Added preferences controller tests

--- test/controllers/public/categories_controller_test.rb
+++ test/controllers/public/categories_controller_test.rb
@@ -3,7 +3,22 @@
 require "test_helper"
 
 class Public::CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = create(:category)
+    @user = build(:user)
+    @article = create(:article, user: @user, assigned_category: @category)
+    @preference = create(:preference, is_password: false)
+    @headers = headers()
+  end
+
+  def test_should_index_all_categories
+    get public_categories_path, headers: headers
+    assert_response :success
+
+    response_json = response.parsed_body
+    all_categories = response_json["categories"]
+    existing_categories_count = Category.count
+
+    assert_equal all_categories.count, existing_categories_count
+  end
 end
