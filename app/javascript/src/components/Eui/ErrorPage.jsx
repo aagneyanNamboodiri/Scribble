6942498f71731b791771920caa850ee93919660f
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-17 23:11:41 +0530

    Added and modified error 404 page

--- app/javascript/src/components/Eui/ErrorPage.jsx
+++ app/javascript/src/components/Eui/ErrorPage.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 
-import { PageLoader, Typography, Button } from "neetoui";
+import { Typography, Button } from "neetoui";
 
 const ErrorPage = ({ error }) => (
   <div className="flex w-full justify-center py-20">
     <div className="flex-col space-y-4">
       <div className=" space-y-10">
-        {error === "404" && (
-          <div className="p-1">
-            <PageLoader />
-          </div>
-        )}
+        <Typography style="h1">:/</Typography>
         <div className="space-y-2">
           <Typography style="h2" weight="medium">
             {error === "404"
-              ? "The eternal loading page (Error 404)"
+              ? "Now Thats a bummer..."
               : "There are no articles published"}
           </Typography>
           <Typography className="text-gray-600" style="body2">
             {error === "404"
-              ? "You are seeing this page because the article doesn't exist."
+              ? "You are seeing this page because this article doesn't exist."
               : "Please come back later"}
           </Typography>
         </div>
