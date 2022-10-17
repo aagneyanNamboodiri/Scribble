f3e654f8a538b09bb6690884b4f957b24fcedf5f
Aagneyan Namboodiri <110370191+aagneyanNamboodiri@users.noreply.github.com>
2022-10-17 16:12:32 +0530

    Preferences controller tests (#77)
    * Added a faker bot for preferences
    
    * Added validations to preference model
    
    * Added preferences controller tests

--- test/controllers/preferences_controller_test.rb
+++ test/controllers/preferences_controller_test.rb
@@ -3,7 +3,29 @@
 require "test_helper"
 
 class PreferencesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @preference = create(:preference)
+  end
+
+  def test_site_name_shouldnt_be_empty
+    @preference.site_name = ""
+    assert_raises ActiveRecord::RecordInvalid do
+      @preference.save!
+    end
+  end
+
+  def test_password_should_exist_if_is_password_is_true
+    @preference.is_password = true
+    @preference.password_digest = ""
+    assert_raises ActiveRecord::RecordInvalid do
+      @preference.save!
+    end
+  end
+
+  def test_password_digest_length_is_atleast_six_characters
+    @preference.password_digest = "close"
+    assert_raises ActiveRecord::RecordInvalid do
+      @preference.save!
+    end
+  end
 end
---
+++ test/factories/preference.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :preference do
+    site_name { Faker::Name.name }
+    is_password { [true, false].sample() }
+    password_digest { Faker::Adjective.positive }
+    authentication_token { Faker::Alphanumeric.alphanumeric(number: 10) }
+  end
+end
