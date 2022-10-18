11f4c4715a01dc688a317ac59cdcce0a4f750ff4
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-18 11:51:37 +0530

    Added tests and improved redirections validation

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Redirection < ApplicationRecord
-  validates :from_path, presence: true
-  validates :to_path, presence: true
+  VALID_PATH_REGEX = /\A
+  [\/]|[\/][a-zA-Z0-9-]+$
+  /x
+
+  validates :from_path, format: VALID_PATH_REGEX, presence: true
+  validates :to_path, format: VALID_PATH_REGEX, presence: true
 end
