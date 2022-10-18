745dc87586e826fe5316794036684d109d6ab3bb
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-18 13:13:44 +0530

    Added loop checking validation and error message in redirections model

--- app/models/redirection.rb
+++ app/models/redirection.rb
@@ -5,6 +5,26 @@ class Redirection < ApplicationRecord
   [\/]|[\/][a-zA-Z0-9-]+$
   /x
 
-  validates :from_path, format: VALID_PATH_REGEX, presence: true
+  validates :from_path, uniqueness: true, format: VALID_PATH_REGEX, presence: true
   validates :to_path, format: VALID_PATH_REGEX, presence: true
+  validate :loop_checking
+
+  def loop_checking
+    all_from_paths = Redirection.pluck(:from_path)
+    all_to_paths = Redirection.pluck(:to_path)
+    possible_from_path = to_path
+    redirection_path = from_path
+    while all_from_paths.include?(possible_from_path) do
+      redirection_path += ("->" + to_path + "->")
+      to_path_of_possible_from_path = Redirection.find_by(from_path: possible_from_path).to_path
+      possible_from_path = to_path_of_possible_from_path
+      redirection_path += possible_from_path
+      if possible_from_path == from_path
+        errors.add(
+          :base,
+          "This redirection forms a loop. Please change the redirection. Loop path : " + redirection_path)
+        break
+      end
+    end
+  end
 end
