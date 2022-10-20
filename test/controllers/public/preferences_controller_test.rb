a46d5383409d949c60868605e470a1b17b87fb0d
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-20 12:11:54 +0530

    Removed a stray byebug from a controller test

--- test/controllers/public/preferences_controller_test.rb
+++ test/controllers/public/preferences_controller_test.rb
@@ -16,7 +16,6 @@ class Public::PreferencesControllerTest < ActionDispatch::IntegrationTest
     site_name = response_json["site_name"]
     password_digest = response_json["password_digest"]
     current_preference = Preference.first
-    byebug
     assert_equal site_name, current_preference.site_name
     assert_equal password_digest, current_preference.password_digest
   end
