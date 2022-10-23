144b568fc909bbb19982b60bb5d57818de38dadf
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-23 14:15:15 +0530

    Added tests to categories controller and modified some

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -7,7 +7,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     @category = create(:category)
     @category_2 = create(:category)
     @user = create(:user)
-    @article = create(:article, user: @user, assigned_category: @category)
+    @article = build(:article, user: @user, assigned_category: @category)
     @headers = headers()
   end
 
@@ -19,7 +19,19 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     all_categories = response_json["categories"]
     existing_categories_count = Category.count
 
-    assert_equal all_categories.count, existing_categories_count
+    assert_equal existing_categories_count, all_categories.count
+  end
+
+  def test_should_create_valid_category
+    post categories_path, params: {
+      category: {
+        name: "random category"
+      }
+    }, headers: headers
+
+    assert_response :success
+    response_json = response.parsed_body
+    assert_equal t("successfully_created", entity: "Category"), response_json["notice"]
   end
 
   def test_shouldnt_delete_category_if_it_has_article_assigned_to_it
@@ -33,37 +45,46 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   def test_should_delete_category_if_it_has_no_articles_under_it
     @category_2.save!
-    delete category_path(@category_2.id.to_i), headers: headers
+    delete category_path(@category_2.id), headers: headers
     response_json = response.parsed_body
+
     assert_response :success
+    assert_equal t("successfully_destroyed", entity: "Category"), response_json["notice"]
   end
 
   def test_should_switch_article_category_to_new_category_when_deleting
-    @article.assigned_category = @category
     @article.save!
-    delete category_path(@category.id.to_i),
+    delete category_path(@category.id),
       params: {
-        new_category: @category_2.id.to_i
+        new_category: @category_2.id
       },
       headers: headers
+
     assert_response :success
+    @article.reload
+    assert_equal @category_2.id, @article.assigned_category_id
+  end
+
+  def test_should_create_general_category_when_deleting_last_category
+    delete category_path(@category_2.id), headers: headers
+    assert_response :success
+
+    @article.save!
+    delete category_path(@category.id), headers: headers
+
+    assert_response :success
+    assert_equal 1, Category.all.count
+    assert_equal "General", Category.first.name
   end
 
   def test_should_updated_category_name
     @category.save!
-    put category_path(@category.id.to_i), params: {
-                                            category:
-                                                  { name: "Death by chocolate" }
-                                          },
+    put category_path(@category.id), params: {
+                                       category:
+                                             { name: "Chocolate" }
+                                     },
       headers: headers
     assert_response :ok
-  end
-
-  def test_shouldnt_create_category_without_name
-    post categories_path, params: { category: { name: "" } }, headers: headers
-    assert_response :unprocessable_entity
-    response_json = response.parsed_body
-    assert_equal response_json["error"], "Name can't be blank"
   end
 
   def test_category_reorder
@@ -71,8 +92,10 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     @category_2.position = 2
     @category.save!
     @category_2.save!
-    put reorder_category_path(@category_2.id.to_i), params: { reorder: { reorder_positions: "-2" } }, headers: headers
-    response_json = response.parsed_body
-    assert_response :no_content
+    put reorder_category_path(@category_2.id), params: { reorder: { positions: 2 } }, headers: headers
+    @category.reload
+    @category_2.reload
+    assert_equal 1, @category_2.position
+    assert_equal 2, @category.position
   end
 end
