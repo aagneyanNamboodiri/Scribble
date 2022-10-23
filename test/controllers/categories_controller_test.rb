1a473de57be562e8f5ccba25cf973c0a82063e05
Aagneyan Namboodiri <110370191+aagneyanNamboodiri@users.noreply.github.com>
2022-10-23 22:57:12 +0530

    Added association tests for user model and category model (#130)

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -34,15 +34,6 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_equal t("successfully_created", entity: "Category"), response_json["notice"]
   end
 
-  def test_shouldnt_delete_category_if_it_has_article_assigned_to_it
-    some_category = build(:category)
-    @article.assigned_category = some_category
-    @article.save!
-    assert_raises NoMethodError do
-      delete category_path(@some_category.id.to_i), headers: headers
-    end
-  end
-
   def test_should_delete_category_if_it_has_no_articles_under_it
     @category_2.save!
     delete category_path(@category_2.id), headers: headers
