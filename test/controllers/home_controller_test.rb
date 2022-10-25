cc4c56dcfc90b858374b265a5258cd761aa00dd3
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-25 18:50:38 +0530

    Added redirection test in home controller

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,12 +4,18 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @organization = create(:organization, is_password: false)
+    @organization = create(:organization)
     @user = create(:user, organization: @organization)
+    @redirection = create(:redirection, user: @user)
   end
 
   def test_should_get_successfully_from_root_url
     get root_path
     assert_response :success
+  end
+
+  def test_should_redirect_to
+    get "/#{@redirection.from_path}"
+    assert_response :moved_permanently
   end
 end
