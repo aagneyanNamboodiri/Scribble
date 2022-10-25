009a74fd932d52af0eb3e6fa101ce461e0b9cf77
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-25 10:56:30 +0530

    Modified tests and added test for login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,11 +3,10 @@
 class LoginController < ApplicationController
   def create
     password = params[:password]
-    @organization = Organization.first
-    if @organization.is_password
-      if @organization.authenticate(password)
-        session[:auth] = @organization.authentication_token
-        respond_with_login_success(t("login_successful", entity: @organization.site_name))
+    if current_organization.is_password
+      if current_organization.authenticate(password)
+        session[:auth] = current_organization.authentication_token
+        respond_with_login_success(t("login_successful", entity: current_organization.site_name))
       else
         respond_with_error(t("incorrect_credentials"))
       end
--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -4,41 +4,51 @@ require "test_helper"
 
 class LoginControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @organization = create(:organization, password_digest: "admin1")
+    @organization = create(:organization, is_password: true, password: "admin1", password_confirmation: "admin1")
   end
 
   def test_wrong_password_should_send_error_response
-    @organization.is_password = true
-    @organization.password_digest = "admin1"
     @organization.save!
-    post login_path, params: { password: "I am the storm that is approaching" },
+    post login_path, params: { password: "notadmin123" },
       headers: headers
     response_json = response.parsed_body
-    assert_equal response_json["error"], "Incorrect credentials, try again."
+
+    assert_equal t("incorrect_credentials"), "Incorrect credentials, try again."
   end
 
   def test_correct_password_should_send_correct_response
-    @organization.is_password = true
-    @organization.password_digest = "admin2"
     @organization.save!
-    post login_path, params: { password: "admin2" },
+    post login_path, params: { password: "admin1" },
       headers: headers
+
     assert_response 201
   end
 
   def test_session_cookie_set_for_correct_password
-    @organization.is_password = true
     @organization.save!
     post login_path, params: { password: "admin1" },
       headers: headers
+
     assert_equal session[:auth].nil?, false
   end
 
   def test_session_cookie_not_set_for_wrong_password
-    @organization.is_password = true
     @organization.save!
-    post login_path, params: { password: "Cus im only a crack in the castle of glass" },
+    post login_path, params: { password: "notadmin123" },
       headers: headers
+    response_json = response.parsed_body
+
+    assert_equal t("incorrect_credentials"), "Incorrect credentials, try again."
     assert_equal session[:auth].nil?, true
+  end
+
+  def test_user_should_be_authorized_regardless_if_password_protection_is_false
+    @organization.is_password = false
+    @organization.save!
+    post login_path, params: {}, headers: headers
+    response_json = response.parsed_body
+
+    assert_equal t("login_successful", entity: @organization.site_name), "Welcome to #{@organization.site_name}"
+    assert_equal true, session[:auth].nil?
   end
 end
