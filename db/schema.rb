c21adc202cc849f7dbf1c7609f57b95512ed8909
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-10-29 13:54:31 +0530

    Migrated pgcrypto

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_180018) do
+ActiveRecord::Schema.define(version: 2022_10_29_082001) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "articles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -63,6 +64,16 @@ ActiveRecord::Schema.define(version: 2022_10_24_180018) do
     t.datetime "updated_at", precision: 6, null: false
     t.uuid "organization_id"
     t.index ["organization_id"], name: "index_users_on_organization_id"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.string "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "articles", "categories", column: "assigned_category_id"
