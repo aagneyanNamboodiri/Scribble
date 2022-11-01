c3b99678fed70eb6930b4be158eecfc11963a7c4
Aagneyan Namboodiri <110370191+aagneyanNamboodiri@users.noreply.github.com>
2022-11-01 23:42:22 +0530

    Adding search to eui (#180)
    * Added modal to search articles
    
    * Added searching frontend activity:
    
    * Added basic article searching

---
+++ app/javascript/src/components/Eui/ArticleSearchModal.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+
+import { Search } from "neetoicons";
+import { Modal, Input, Typography, Tag } from "neetoui";
+import { Link } from "react-router-dom";
+
+import publicArticlesApi from "apis/Public/articles";
+
+const ArticleSearchModal = ({ isSearchModalOpen, setIsSearchModalOpen }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    const payload = { search_term: searchTerm };
+    try {
+      const {
+        data: { articles },
+      } = await publicArticlesApi.list(payload);
+      setArticles(articles);
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const getData = setTimeout(() => {
+      if (searchTerm.length > 0) fetchArticles();
+    }, 400);
+
+    return () => clearTimeout(getData);
+  }, [searchTerm]);
+
+  return (
+    <div className="w-full">
+      <Modal
+        closeOnEsc
+        closeButton={false}
+        isOpen={isSearchModalOpen}
+        onClose={() => setIsSearchModalOpen(false)}
+      >
+        <Input
+          className="w-full"
+          placeholder="Search for article title"
+          prefix={<Search />}
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        {searchTerm.length > 0 &&
+          articles.map((article, idx) => (
+            <Link
+              a
+              className="w-10/12 p-2"
+              id={idx}
+              key={article.id}
+              to={`/public/${article.slug}`}
+              onClick={() => setIsSearchModalOpen(false)}
+            >
+              <div
+                className="mb-3 flex justify-between px-2 hover:text-indigo-500"
+                onClick={() => setIsSearchModalOpen(false)}
+              >
+                <Typography className="" style="h4">
+                  {article.title}
+                </Typography>
+                <Tag label={article.category_name} />
+              </div>
+            </Link>
+          ))}
+      </Modal>
+    </div>
+  );
+};
+export default ArticleSearchModal;
