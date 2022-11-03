28563162684ac68f93c41cc1fb4d6a4a1fe15377
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-11-03 21:39:15 +0530

    Minor changes to public articles controller

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -4,17 +4,18 @@ class Public::ArticlesController < Public::BaseController
   before_action :check_if_user_is_authorized
 
   def index
-    @articles = current_user.articles.all.where(status: "published").where(
-      "lower(title) like ?",
-      "%#{params[:search_term].downcase}%")
+    @articles = get_published_articles
   end
 
   def show
-    @article = current_user.articles.where(status: "published").find_by(slug: params[:slug])
+    @article = get_published_articles.find_by(slug: params[:slug])
     if !@article
       respond_with_error(t("doesnt_exist", entity: "Article"))
     end
-
     @article.increment!(:visits)
+  end
+
+  def get_published_articles
+    published_articles = current_user.articles.all.where(status: "published")
   end
 end
