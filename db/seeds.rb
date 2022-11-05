3ea859008e6e3b82984ec46936e50350c6a64ac3
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-11-05 19:07:34 +0530

    Modified setup.rake and seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,67 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-current_user = User.first
-
-Article.destroy_all
-p "Destroyed all articles"
-
-Category.destroy_all
-p "Destroyed all categories"
-
-5.times do |index|
-  current_user.categories.create!(
-    name: Faker::Commerce.unique.department(max: 1)
-  )
+def create_sample_data!
+  create_organization!
+  create_user!
+  seed_data!
+  seed_frontend_routes!
 end
 
-p "Created #{current_user.categories.count} categories"
-
-20.times do |index|
-  current_user.articles.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph(sentence_count: rand(10..50)),
-    assigned_category: Category.find(current_user.categories.pluck(:id).sample),
-    status: ["draft", "published"].sample()
-  )
+def create_organization!(options = {})
+  organization_attributes = {
+    site_name: "Spinkart",
+    is_password: false
+  }
+  attributes = organization_attributes.merge options
+  organization = Organization.new(attributes)
+  organization.save!(validations: false)
 end
 
-p "Created #{current_user.articles.count} articles"
+def create_user!(options = {})
+  user_attributes = {
+    name: "Oliver Smith"
+  }
+  attributes = user_attributes.merge options
+  Organization.first.create_user! attributes
+end
+
+def seed_data!
+  Article.reset_column_information
+  Redirection.reset_column_information
+  Category.reset_column_information
+
+  current_user = User.first
+
+  Article.destroy_all
+  p "Destroyed all articles"
+
+  Category.destroy_all
+  p "Destroyed all categories"
+
+  5.times do |index|
+    current_user.categories.create!(
+      name: Faker::Commerce.unique.department(max: 1)
+    )
+  end
+
+  p "Created #{current_user.categories.count} categories"
+
+  20.times do |index|
+    current_user.articles.create!(
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph(sentence_count: rand(10..50)),
+      assigned_category: Category.find(current_user.categories.pluck(:id).sample),
+      status: ["draft", "published"].sample()
+    )
+  end
+
+  p "Created #{current_user.articles.count} articles"
+end
+
+def seed_frontend_routes!
+  FrontendRoute.create!(route: "articles")
+  FrontendRoute.create!(route: "analytics")
+  FrontendRoute.create!(route: "settings")
+  FrontendRoute.create!(route: "public")
+end
