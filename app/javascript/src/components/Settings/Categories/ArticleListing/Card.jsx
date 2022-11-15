2e447a44627021fa2fad4270be63db2c71e7afd3
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-11-15 18:34:06 +0530

    Formatted article listing card

--- app/javascript/src/components/Settings/Categories/ArticleListing/Card.jsx
+++ app/javascript/src/components/Settings/Categories/ArticleListing/Card.jsx
@@ -4,13 +4,13 @@ import { Clock } from "neetoicons";
 import { Checkbox, Typography, Tag } from "neetoui";
 import TooltipWrapper from "tooltipwrapper";
 
-import { formatTime } from "../utils";
+import { daysFromNowFormat, formatTime } from "../utils";
 
 const Card = ({ article }) => {
   const [checked, setChecked] = useState(false);
 
   return (
-    <div className="border m-3 w-full space-y-2 p-4 shadow-md">
+    <div className="border m-3 w-full space-y-2 p-4 hover:shadow-md">
       <Checkbox checked={checked} onChange={() => setChecked(prev => !prev)} />
       <div className="flex justify-between">
         <Typography style="h3">{article.title}</Typography>
@@ -19,20 +19,24 @@ const Card = ({ article }) => {
         <Typography style="body2">{article.body}</Typography>
       </div>
       <hr />
-      <div className="flex w-full justify-between">
-        <Tag className="bg-gray-100" label="Getting Started" />
-        <div className="flex justify-between pt-1">
-          <Clock size={18} />
-          <TooltipWrapper
-            content="2 hours ago"
-            followCursor="horizontal"
-            position="bottom"
-          >
-            <Typography className="pr-2 text-gray-500" style="body3">
-              {formatTime(article.updated_at)}
-            </Typography>
-          </TooltipWrapper>
-        </div>
+      <div className="flex justify-end space-x-2 pt-1">
+        <Clock size={18} />
+        <TooltipWrapper
+          disabled
+          content={formatTime(article.updated_at)}
+          followCursor="horizontal"
+          position="bottom"
+        >
+          <Typography className="pr-2 text-gray-500" style="body3">
+            {daysFromNowFormat(article.updated_at)}
+          </Typography>
+        </TooltipWrapper>
+        <Tag
+          className="bg-gray-100"
+          label={article.status === "published" ? "Published" : "Drafted"}
+          style={article.status === "published" ? "success" : "warning"}
+          type="solid"
+        />
       </div>
     </div>
   );
--- app/javascript/src/components/Settings/Categories/utils.js
+++ app/javascript/src/components/Settings/Categories/utils.js
@@ -2,11 +2,15 @@ import dayjs from "dayjs";
 import * as yup from "yup";
 
 var advancedFormat = require("dayjs/plugin/advancedFormat");
+var relativeTime = require("dayjs/plugin/relativeTime");
 
 dayjs.extend(advancedFormat);
+dayjs.extend(relativeTime);
 
 export const formatTime = dateTime =>
   dateTime === "-" ? "-" : dayjs(dateTime).format("MMMM Do, YYYY");
+
+export const daysFromNowFormat = dateTime => dayjs(dateTime).fromNow();
 
 export const buildDeleteModalSelectValidationSchema = categoryList => {
   const validationSchema = yup.object().shape({
