879afa88b50ef08c3f970a15402b8b3cfcbdd689
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-11-16 23:09:00 +0530

    Fixes tests

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::CategoriesController < ApplicationController
   before_action :load_category!, except: %i[index create]
-  before_action :set_to_category, only: %i[delete]
+  before_action :set_to_category, only: %i[destroy]
 
   def index
     @categories = current_user.categories.all.order(position: :asc)
@@ -14,7 +14,9 @@ class Api::CategoriesController < ApplicationController
   end
 
   def destroy
-    SwitchArticlesToNewCategoryService.new(@category.articles.ids, @to_category).process
+    if @category.articles.count > 0
+      SwitchArticlesToNewCategoryService.new(@category.articles.ids, @to_category_id).process
+    end
     @category.destroy!
     respond_with_success(t("successfully_destroyed", entity: "Category"))
   end
@@ -48,9 +50,9 @@ class Api::CategoriesController < ApplicationController
     end
 
     def set_to_category
-      @to_category = params[:new_category]
-      if @current_user.categories.count == 1
-        @to_category = @current_user.categories.create!(name: "General").id
+      @to_category_id = params[:new_category]
+      if current_user.categories.count == 1
+        @to_category_id = current_user.categories.create!(name: "General").id
       end
     end
 end
