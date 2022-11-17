dc2f19649936ddc07888d565c2cea28efd62789e
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-11-17 20:03:18 +0530

    Added a service to delete categories

---
+++ app/services/delete_category_service.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class DeleteCategoryService
+  def initialize(category_to_be_deleted, category_to_put_articles_to)
+    @current_user = User.first
+    @category_to_be_deleted = category_to_be_deleted
+    @category_to_put_articles_to = category_to_put_articles_to
+    if @category_to_be_deleted == @category_to_put_articles_to
+      raise Exception.new "Cannot delete the category to switch articles to."
+    end
+  end
+
+  def process
+    delete_category_service
+  end
+
+  private
+
+    def delete_category_service
+      if @current_user.categories.count == 1
+        if @current_user.categories.first.name == "General"
+          raise Exception.new t("cannot_delete_general_category")
+        else
+          @category_to_put_articles_to = @current_user.categories.create!(name: "General").id
+        end
+      end
+      articles_to_switch_categories =
+        @current_user.articles.where(assigned_category_id: @category_to_be_deleted)
+      articles_to_switch_categories.each do |article|
+        article.assigned_category_id = @category_to_put_articles_to
+        article.save!
+      end
+      @current_user.categories.find(@category_to_be_deleted).destroy!
+    end
+end
