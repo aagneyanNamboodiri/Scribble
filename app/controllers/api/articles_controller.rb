9964c09287803d2dcd91859e15cac09dcb956307
Aagneyan Namboodiri <110370191+aagneyanNamboodiri@users.noreply.github.com>
2022-11-18 20:16:25 +0530

    Service for filtering articles (#232)
    * Added service to filter articles
    
    * Added tests for article filtering service

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,17 +2,13 @@
 
 class Api::ArticlesController < ApplicationController
   before_action :load_article!, only: %i[show update destroy reorder]
-  before_action :search_params, only: %i[index]
+  before_action :extract_search_params, only: %i[index]
   before_action :load_category!, only: %i[articles_of_category]
 
   def index
-    @articles = filter_articles
-    if @categories_to_filter_with.length == 0
-      render
-    end
-
-    @articles = @articles.select { |article|
-      @categories_to_filter_with.include? article.assigned_category.name }
+    @articles = ArticleFilteringService.new(
+      @categories_to_filter_with, @status_to_filter, @search_term,
+      current_user).process
   end
 
   def create
@@ -49,7 +45,7 @@ class Api::ArticlesController < ApplicationController
 
   private
 
-    def search_params
+    def extract_search_params
       @search_term = params[:search_query] || ""
       @categories_to_filter_with = params[:selected_category_fiter] || []
       @status_to_filter = params[:article_status] || "all"
@@ -61,14 +57,6 @@ class Api::ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit([:title, :body, :assigned_category_id, :status, :restored_from])
-    end
-
-    def filter_articles
-      search_query_filtered_articles = current_user.articles.where(
-        "lower(title) like ?",
-        "%#{@search_term.downcase}%").order(updated_at: :desc)
-      status_filtered_articles = search_query_filtered_articles.select { |article|
-      @status_to_filter == "all" || article.status == @status_to_filter }
     end
 
     def load_category!
