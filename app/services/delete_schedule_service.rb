0d32a52e63d83978250bae8dd3720057989cb173
aagneyanNamboodiri <aagneyan.namboodiri@bigbinary.com>
2022-11-25 20:48:18 +0530

    Made service to delete a schedule

---
+++ app/services/delete_schedule_service.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class DeleteScheduleService
+  attr_reader :id, :current_user
+
+  def initialize(id, current_user)
+    @id = id
+    @current_user = current_user
+  end
+
+  def process
+    delete_schedule_service
+  end
+
+  private
+
+    def delete_schedule_service
+      schedule = ArticleStatusSchedule.find(id)
+      article = current_user.articles.find(schedule.article_id)
+      schedules_for_article = article.article_status_schedules.all.order(scheduled_time: :desc)
+      duplicate_schedule = schedules_for_article.find_by!(scheduled_time: schedule.scheduled_time..)
+      if schedule.id != duplicate_schedule.id
+        duplicate_schedule.destroy!
+      end
+      schedule.destroy!
+    end
+end
